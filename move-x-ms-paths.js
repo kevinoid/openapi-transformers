8174f1afa1008273845190225862dd46c9b4999e
Kevin Locke <kevin@kevinlocke.name>
2021-05-10 11:12:42 -0600

    MoveXMsPaths extends OpenApiTransformerBase
    For consistency.
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- move-x-ms-paths.js
+++ move-x-ms-paths.js
@@ -10,34 +10,40 @@
 
 import assert from 'assert';
 
-function moveXMsPaths(spec) {
-  const queryPaths = Object.keys(spec.paths)
-    .filter((path) => path.includes('?'));
-
-  if (queryPaths.length === 0) {
-    return spec;
-  }
-
-  const paths = { ...spec.paths };
-  const xMsPaths = { ...spec['x-ms-paths'] };
-  for (const path of queryPaths) {
-    assert(
-      !hasOwnProperty.call(xMsPaths, path),
-      `${path} already present in x-ms-paths`,
-    );
-    xMsPaths[path] = paths[path];
-    delete paths[path];
-  }
-
-  return {
-    ...spec,
-    paths,
-    'x-ms-paths': xMsPaths,
-  };
-}
+import OpenApiTransformerBase from 'openapi-transformer-base';
 
 // eslint-disable-next-line import/no-unused-modules
-export default function factory(options) {
-  // TODO: Inherit from OpenApiTransformerBase
-  return { transformOpenApi: moveXMsPaths };
+export default class MoveXMsPathsTransformer
+  extends OpenApiTransformerBase {
+  // Override as performance optimization, since only transforming paths
+  // eslint-disable-next-line class-methods-use-this
+  transformOpenApi(spec) {
+    if (!spec || !spec.paths) {
+      return spec;
+    }
+
+    const queryPaths = Object.keys(spec.paths)
+      .filter((path) => path.includes('?'));
+
+    if (queryPaths.length === 0) {
+      return spec;
+    }
+
+    const paths = { ...spec.paths };
+    const xMsPaths = { ...spec['x-ms-paths'] };
+    for (const path of queryPaths) {
+      assert(
+        !hasOwnProperty.call(xMsPaths, path),
+        `${path} already present in x-ms-paths`,
+      );
+      xMsPaths[path] = paths[path];
+      delete paths[path];
+    }
+
+    return {
+      ...spec,
+      paths,
+      'x-ms-paths': xMsPaths,
+    };
+  }
 }
