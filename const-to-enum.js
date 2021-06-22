f294f4bbf19bf93a926ca64e685ba41ea6926456
Kevin Locke <kevin@kevinlocke.name>
2021-06-22 10:20:56 -0600

    feat: create ConstToEnumTransformer
    For transforming Schema Objects with a `const` constraint (specified for
    OAS 3.1 and recent JSON Schema) to an `enum` constraint (specified for
    all).
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

---
+++ const-to-enum.js
@@ -0,0 +1,49 @@
+/**
+ * @copyright Copyright 2021 Kevin Locke <kevin@kevinlocke.name>
+ * @license MIT
+ * @module "openapi-transformers/const-to-enum.js"
+ */
+
+import OpenApiTransformerBase from 'openapi-transformer-base';
+
+/**
+ * Transform Schema Objects with `const` (as in OAS 3.1 and JSON Schema)
+ * to `enum` for OAS 3.0 and 2.0.
+ */
+export default class ConstToEnumTransformer extends OpenApiTransformerBase {
+  transformSchema(schema) {
+    const newSchema = super.transformSchema(schema);
+    if (newSchema === null
+      || typeof newSchema !== 'object'
+      || Array.isArray(newSchema)) {
+      return newSchema;
+    }
+
+    const { const: constValue, ...schemaNoConst } = newSchema;
+    if (constValue === undefined) {
+      return newSchema;
+    }
+
+    const enumValues = schemaNoConst.enum;
+    if (enumValues === undefined) {
+      schemaNoConst.enum = [constValue];
+    } else if (Array.isArray(enumValues)) {
+      if (enumValues.includes(constValue)) {
+        // Schema validation would only succeed for const value.
+        // Safe to overwrite.
+        schemaNoConst.enum = [constValue];
+      } else if (enumValues.length > 0) {
+        // Schema validation would always fail (because either const or enum
+        // constraint would be unsatisfied).  Replace with empty enum, which
+        // also always fails.
+        this.warn(
+          'Using empty enum for schema with const not in enum',
+          newSchema,
+        );
+        schemaNoConst.enum = [];
+      }
+    }
+
+    return schemaNoConst;
+  }
+}
---
+++ test/const-to-enum.js
@@ -0,0 +1,155 @@
+/**
+ * @copyright Copyright 2021 Kevin Locke <kevin@kevinlocke.name>
+ * @license MIT
+ */
+
+import assert from 'assert';
+import deepFreeze from 'deep-freeze';
+
+import ConstToEnumTransformer from '../const-to-enum.js';
+
+describe('ConstToEnumTransformer', () => {
+  it('const: 1 to enum: [1]', () => {
+    assert.deepStrictEqual(
+      new ConstToEnumTransformer().transformOpenApi(deepFreeze({
+        openapi: '3.1.0',
+        info: {
+          title: 'Title',
+          version: '1.0',
+        },
+        components: {
+          schemas: {
+            Example: {
+              const: 1,
+            },
+          },
+        },
+        paths: {},
+      })),
+      {
+        openapi: '3.1.0',
+        info: {
+          title: 'Title',
+          version: '1.0',
+        },
+        components: {
+          schemas: {
+            Example: {
+              enum: [1],
+            },
+          },
+        },
+        paths: {},
+      },
+    );
+  });
+
+  it('const: null to enum: [null]', () => {
+    assert.deepStrictEqual(
+      new ConstToEnumTransformer().transformOpenApi(deepFreeze({
+        openapi: '3.1.0',
+        info: {
+          title: 'Title',
+          version: '1.0',
+        },
+        components: {
+          schemas: {
+            Example: {
+              const: null,
+            },
+          },
+        },
+        paths: {},
+      })),
+      {
+        openapi: '3.1.0',
+        info: {
+          title: 'Title',
+          version: '1.0',
+        },
+        components: {
+          schemas: {
+            Example: {
+              enum: [null],
+            },
+          },
+        },
+        paths: {},
+      },
+    );
+  });
+
+  // const and enum on the same type is redundant, but valid (AFAICT)
+  it('removes const with matching enum', () => {
+    assert.deepStrictEqual(
+      new ConstToEnumTransformer().transformOpenApi(deepFreeze({
+        openapi: '3.1.0',
+        info: {
+          title: 'Title',
+          version: '1.0',
+        },
+        components: {
+          schemas: {
+            Example: {
+              const: 1,
+              enum: [1],
+            },
+          },
+        },
+        paths: {},
+      })),
+      {
+        openapi: '3.1.0',
+        info: {
+          title: 'Title',
+          version: '1.0',
+        },
+        components: {
+          schemas: {
+            Example: {
+              enum: [1],
+            },
+          },
+        },
+        paths: {},
+      },
+    );
+  });
+
+  // since const value is only one which satisfies both constraints
+  it('narrows matching enum to only const value', () => {
+    assert.deepStrictEqual(
+      new ConstToEnumTransformer().transformOpenApi(deepFreeze({
+        openapi: '3.1.0',
+        info: {
+          title: 'Title',
+          version: '1.0',
+        },
+        components: {
+          schemas: {
+            Example: {
+              const: null,
+              enum: [1, null],
+            },
+          },
+        },
+        paths: {},
+      })),
+      {
+        openapi: '3.1.0',
+        info: {
+          title: 'Title',
+          version: '1.0',
+        },
+        components: {
+          schemas: {
+            Example: {
+              enum: [null],
+            },
+          },
+        },
+        paths: {},
+      },
+    );
+  });
+});
