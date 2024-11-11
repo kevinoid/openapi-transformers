c6dc35fd0cf5535c0692874b3d3e518987d885c0
Kevin Locke <kevin@kevinlocke.name>
2024-11-11 10:27:21 -0700

    test: allOf/anyOf/oneOf NullableNotRequired
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- test/nullable-not-required.js
+++ test/nullable-not-required.js
@@ -58,4 +58,245 @@ describe('NullableNotRequiredTransformer', () => {
       }),
     );
   });
+
+  it('nullable required allOf property to not required', () => {
+    assert.deepStrictEqual(
+      new NullableNotRequiredTransformer().transformOpenApi(deepFreeze(schema3({
+        allOf: [
+          {
+            type: 'object',
+            properties: {
+              name: {
+                type: 'string',
+                nullable: true,
+              },
+            },
+          },
+        ],
+        required: ['name'],
+      }))),
+      schema3({
+        allOf: [
+          {
+            type: 'object',
+            properties: {
+              name: {
+                type: 'string',
+                nullable: true,
+              },
+            },
+          },
+        ],
+      }),
+    );
+  });
+
+  it('some nullable required allOf property still required', () => {
+    assert.deepStrictEqual(
+      new NullableNotRequiredTransformer().transformOpenApi(deepFreeze(schema3({
+        allOf: [
+          {
+            type: 'object',
+            properties: {
+              name: {
+                type: 'string',
+                nullable: true,
+              },
+            },
+          },
+          {
+            type: 'object',
+            properties: {
+              name: {
+                type: 'string',
+              },
+            },
+          },
+        ],
+        required: ['name'],
+      }))),
+      schema3({
+        allOf: [
+          {
+            type: 'object',
+            properties: {
+              name: {
+                type: 'string',
+                nullable: true,
+              },
+            },
+          },
+          {
+            type: 'object',
+            properties: {
+              name: {
+                type: 'string',
+              },
+            },
+          },
+        ],
+        required: ['name'],
+      }),
+    );
+  });
+
+  it('nullable required anyOf property to not required', () => {
+    assert.deepStrictEqual(
+      new NullableNotRequiredTransformer().transformOpenApi(deepFreeze(schema3({
+        anyOf: [
+          {
+            type: 'object',
+            properties: {
+              name: {
+                type: 'string',
+                nullable: true,
+              },
+            },
+          },
+        ],
+        required: ['name'],
+      }))),
+      schema3({
+        anyOf: [
+          {
+            type: 'object',
+            properties: {
+              name: {
+                type: 'string',
+                nullable: true,
+              },
+            },
+          },
+        ],
+      }),
+    );
+  });
+
+  it('some nullable required anyOf property still required', () => {
+    assert.deepStrictEqual(
+      new NullableNotRequiredTransformer().transformOpenApi(deepFreeze(schema3({
+        anyOf: [
+          {
+            type: 'object',
+            properties: {
+              name: {
+                type: 'string',
+                nullable: true,
+              },
+            },
+          },
+          {
+            type: 'object',
+            properties: {
+              name: {
+                type: 'string',
+              },
+            },
+          },
+        ],
+        required: ['name'],
+      }))),
+      schema3({
+        anyOf: [
+          {
+            type: 'object',
+            properties: {
+              name: {
+                type: 'string',
+                nullable: true,
+              },
+            },
+          },
+          {
+            type: 'object',
+            properties: {
+              name: {
+                type: 'string',
+              },
+            },
+          },
+        ],
+      }),
+    );
+  });
+
+  it('nullable required oneOf property to not required', () => {
+    assert.deepStrictEqual(
+      new NullableNotRequiredTransformer().transformOpenApi(deepFreeze(schema3({
+        oneOf: [
+          {
+            type: 'object',
+            properties: {
+              name: {
+                type: 'string',
+                nullable: true,
+              },
+            },
+          },
+        ],
+        required: ['name'],
+      }))),
+      schema3({
+        oneOf: [
+          {
+            type: 'object',
+            properties: {
+              name: {
+                type: 'string',
+                nullable: true,
+              },
+            },
+          },
+        ],
+      }),
+    );
+  });
+
+  it('some nullable required oneOf property still required', () => {
+    assert.deepStrictEqual(
+      new NullableNotRequiredTransformer().transformOpenApi(deepFreeze(schema3({
+        oneOf: [
+          {
+            type: 'object',
+            properties: {
+              name: {
+                type: 'string',
+                nullable: true,
+              },
+            },
+          },
+          {
+            type: 'object',
+            properties: {
+              name: {
+                type: 'string',
+              },
+            },
+          },
+        ],
+        required: ['name'],
+      }))),
+      schema3({
+        oneOf: [
+          {
+            type: 'object',
+            properties: {
+              name: {
+                type: 'string',
+                nullable: true,
+              },
+            },
+          },
+          {
+            type: 'object',
+            properties: {
+              name: {
+                type: 'string',
+              },
+            },
+          },
+        ],
+      }),
+    );
+  });
 });
