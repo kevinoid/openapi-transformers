a33c126af53a8dce02fc6452890ba21f3eecd39e
Kevin Locke <kevin@kevinlocke.name>
2025-01-08 10:38:52 -0700

    Add githook to check exports
    To ensure the exports are updated when changes are committed.
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

---
+++ .githooks/pre-commit/03-check-exports.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+/**
+ * @copyright Copyright 2025 Kevin Locke <kevin@kevinlocke.name>
+ * @license MIT
+ */
+
+import checkExports from '../../script-lib/check-exports.js';
+
+process.exitCode = await checkExports(process.argv, process);
